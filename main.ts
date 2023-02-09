f889faad9d412c22cc3957487004251e19e179fc
martinaaloopez <122534401+martinaaloopez@users.noreply.github.com>
2023-02-09 13:54:53 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    comotuquieras = 0
+})
 radio.onReceivedString(function (receivedString) {
     led.plot(parseFloat(receivedString.substr(0, 1)), parseFloat(receivedString.substr(1, 1)))
 })
+input.onButtonPressed(Button.B, function () {
+    uña = 0
+})
+let uña = 0
+let comotuquieras = 0
 radio.setGroup(1)

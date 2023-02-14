7bdf00c584e8fa2f5fa05b6f97a3fb7a98969f20
martinaaloopez <122534401+martinaaloopez@users.noreply.github.com>
2023-02-14 13:53:42 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    comotuquieras = 0
+    X += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.setGroup(X * 10 + Y)
+    radio.sendString("Martina")
+    radio.setGroup(1)
+    Y = 0
+    X = 0
 })
 radio.onReceivedString(function (receivedString) {
     led.plot(parseFloat(receivedString.substr(0, 1)), parseFloat(receivedString.substr(1, 1)))
 })
 input.onButtonPressed(Button.B, function () {
-    uña = 0
+    Y += 1
 })
-let uña = 0
-let comotuquieras = 0
+let X = 0
+let Y = 0
 radio.setGroup(1)
+Y = 0
+X = 0
